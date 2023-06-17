39d3fbb8c21aa6cdb1066bf8258648eb125e4c8b
javierpomachagua <javierpp0303@gmail.com>
2023-06-17 17:03:11 -0500

    video 7

---
+++ database/migrations/2023_06_17_211059_create_reviews_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\User::class);
+            $table->foreignIdFor(\App\Models\Book::class);
+            $table->string('title');
+            $table->text('description');
+            $table->integer('stars');
+            $table->boolean('is_approved')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
