a3bf5ca877eef7bacfaf33410e9329113305e751
javierpomachagua <javierpp0303@gmail.com>
2023-07-10 18:44:41 -0500

    video 10

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
@@ -14,5 +15,10 @@ class Review extends Model
     public function book()
     {
         return $this->belongsTo(Book::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
