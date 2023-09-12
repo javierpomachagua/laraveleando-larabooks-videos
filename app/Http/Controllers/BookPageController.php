6e5f383012fabe77a648d1130aa9032b7fa72616
javierpomachagua <javierpp0303@gmail.com>
2023-09-12 13:53:59 -0500

    video 20

--- app/Http/Controllers/BookPageController.php
+++ app/Http/Controllers/BookPageController.php
@@ -3,27 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
-use App\Models\Review;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class BookPageController extends Controller
 {
-    public function __invoke(Request $request)
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Book $book)
     {
-        $books = Book::query()
-            ->with(['authors', 'genres'])
-            ->withAvg('reviews', 'stars')
-            ->withCount('reviews')
-            ->when(request('search'), function (Builder $query, $value) {
-                $query->whereRaw('books.name LIKE ?', ["%{$value}%"])
-                    ->orWhereHas('authors', function (Builder $query) use ($value) {
-                        $query->whereRaw('authors.name LIKE ?', ["%{$value}%"]);
-                    });
-            })
-            ->paginate(8);
+        $book->loadAvg('reviews', 'stars')
+            ->loadCount('reviews');
 
-        return view('books', compact('books'));
+        return view('books.show', compact('book'));
     }
 }
---
+++ app/Http/Controllers/BooksPageController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use App\Models\Review;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class BooksPageController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $books = Book::query()
+            ->with(['authors', 'genres'])
+            ->withAvg('reviews', 'stars')
+            ->withCount('reviews')
+            ->when(request('search'), function (Builder $query, $value) {
+                $query->whereRaw('books.name LIKE ?', ["%{$value}%"])
+                    ->orWhereHas('authors', function (Builder $query) use ($value) {
+                        $query->whereRaw('authors.name LIKE ?', ["%{$value}%"]);
+                    });
+            })
+            ->paginate(8);
+
+        return view('books', compact('books'));
+    }
+}
